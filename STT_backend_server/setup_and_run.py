553ebae9563b067c02f586793751782783472771
KJCA\Administrator <fghjklvmbn@naver.com>
2026-06-11 13:44:33 +0900

    코드 개선

--- STT_backend_server/setup_and_run.py
+++ STT_backend_server/setup_and_run.py
@@ -98,10 +98,10 @@ def main():
             print(f"[{target_env}] 환경이 이미 존재하므로 이후 코드를 건너뜁니다.")
         else:
             print(f"[{target_env}] 환경이 없습니다. 새로 생성을 시작합니다...")
-            run_command(command="conda create -n STT python=3.12 -y")
+            run_command(command=f"conda create -n {target_env} python=3.12 -y")
             
         # 가상환경 실행
-        run_command(command="conda activate STT")
+        run_command(command=f"conda activate {target_env}")
 
     # 1. ffmpeg 체크 및 설치(현재 사용자가 "kjca"인 경우 건너뛰기)
     if platform.uname().node == "kjca":
